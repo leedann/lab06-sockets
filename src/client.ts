bf546ed59d1ffd0e5add08c9f3207c9310213472
Danny Lee <leedann@uw.edu>
2017-02-22 12:14:20 -0800

    finished lab

--- src/client.ts
+++ src/client.ts
@@ -9,11 +9,13 @@ const net = require('net'),
     
 
 client.on('data', function(data) { //when we get data
-    
+    console.log(`Received: ${data} \n`)
 });
 
 client.on('close', function() { //when connection closed
-    
+    console.log(`server disconnect`);
+    console.log(`closing client`);
+    process.exit(0);
 });
 
 
@@ -22,6 +24,28 @@ var PORT = 3000;
 
 //connect to the server
 client.connect(PORT, HOST, function() {
+    console.log(`connected to` + HOST + ':' + PORT);
+
+    io.setPrompt('> ');
+    io.prompt();
+
+    io.on('line', function(line) {
+        switch(line.trim()) {
+            case 'exit':
+                client.end();
+                console.log('client disconnect');
+                process.exit(0);
+                break;
+            default:
+                client.write(line);
+                break;
+        }
+        io.prompt();
+    }).on('close', function() {
+        client.end();
+        console.log(`client disconnect`);
+        process.exit(0);
+    });
 
 });
 
--- src/server.ts
+++ src/server.ts
@@ -7,17 +7,48 @@ interface Address { port: number; family: string; address: string; };
 // create socket server
 let server:net.Server = net.createServer();
 
+let clients:net.Socket[] = [];
 // when the server is connected
 server.on('connection', function(socket:net.Socket){
+    function broadcast(name:String, message:String) {
+        clients.forEach(function(client:net.Socket) {
+            if (client != socket) {
+                client.write('[' + name + ']' + message + '\n');
+            }
+        });
+    }
+
+    console.log('connected: ' + socket.remoteAddress);
+    clients.push(socket);
+    socket.write('Hi dood whats your name? \n');
+    let name:String = '';
 
     // when data is sent to the socket
     socket.on('data', function(data){
-        //
-    });
+        let message:String = data.toString();
+        if (message.length == 0) {
+            socket.write(`(you gotta type something and hit return)\n`);
+            return;
+        }
+        if (!name) {
+            name = data.toString().substr(0, 10);
 
-    socket.on('close', function(){
-        // handle client disconnecting
-    })
+            socket.write('Hello ' + name + '!\n');
+            socket.write('Welcome to the chat room, ' + name + '!\n');
+            socket.write('There are ' + clients.length + ' people here.\n');
+            socket.write("Type messages, or type 'exit' at any time to leave.\n");
+        }
+        else {
+            if ('exit' === message) {
+                socket.end();
+            }
+            else {
+                broadcast(name, message);
+            }
+        }
+    });
+});
+
 
 
 });
@@ -26,6 +57,7 @@ server.on('connection', function(socket:net.Socket){
 server.on('listening', function() {
     //output to the console the port number on which we are listening
     var addr:Address = server.address();
+    console.log(addr.address);
     console.log('server listening on port %d', addr.port);
 });
 
